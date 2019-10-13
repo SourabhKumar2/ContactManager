3e42b916786277600d8e549173f8a45e3ddf4135
Sourabh Kumar <sourabhkumar2405@gmail.com>
2019-10-13 23:41:04 +0530

    Add Login / Logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::any('/', 'WebsiteController');
+
+Route::post('login', 'Auth\LoginController@login')->name('login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
@@ -14,4 +18,51 @@ class LoginTest extends TestCase
         $this->get('/')
             ->assertRedirect('login');
     }
+
+    /**
+     * @test
+     */
+    public function user_can_login_with_valid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'password')
+        ]);
+
+        $this->post('login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+    }
+
+    /**
+     * @test
+     */
+    public function user_cannot_login_with_invalid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'password')
+        ]);
+
+        $this->post('login', [
+            'email' => $user->email,
+            'password' => 'invalid password'
+        ])->assertRedirect('/');
+    }
+
+    /**
+     * @test
+     */
+    public function authenticated_user_can_logout()
+    {
+        $user = factory(User::class)->create();
+
+        $this->be($user)->post('logout')
+            ->assertRedirect('/');
+
+        $this->assertFalse($this->isAuthenticated());
+    }
+
 }
